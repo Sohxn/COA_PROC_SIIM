b17f0868f13e4bf528443667102d24f72ad2e22f
Stuti230904 <86624764+Stuti230904@users.noreply.github.com>
2023-11-23 22:42:11 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,19 @@
+import MemoryControl
+import Decoder
+class Main:
+    main_memory = [[],[]]
+    registers = (0,0,0,0,0,0,0,0)
+    PC = 0
+    instr_cnt=0;
+    f = open("COA_Project\instructions.txt","r")
+    instruction=f.readlines()
+    obj1=MemoryControl.MemoryControl(instruction,main_memory)
+    ##list of instructions is sent to MemoryControl which \
+        # saves the instructions to main memory
+    ##list of instructions stored in main_memory[0]
+
+    for i in range (len(instruction)):
+        obj2 = Decoder.decoder(main_memory[0][PC])
+        PC+=1
+    ##loop runs for every instruction
+    ##each instruction is sent to decoder
---
+++ MemoryControl.py
@@ -0,0 +1,3 @@
+class MemoryControl:
+    def __init__(self,a,b):
+        b[0]=a
