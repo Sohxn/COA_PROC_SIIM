db2f43d46f5be9dab1664c28bfa9573f47c33133
Stuti230904 <86624764+Stuti230904@users.noreply.github.com>
2023-11-25 23:35:49 +0530

    Update ALU.py

--- ALU.py
+++ ALU.py
@@ -4,22 +4,17 @@ class ALU:
     def __init__(self,a):
         if a[2] == '0':                   #register
             index =int((a[4])[3])*1+int((a[4])[2])*2+int((a[4])[1])*4+int((a[4])[0])*8
-            print(index)
             self.op2=Main.registers[index]
         else:                             #immediate value(in 2's complement)
             if a[4][0] == '1':              #if negative, taking 2's compliment
                 x=(1111-int(a[4])+1)
                 bin = f'{x:04d}'
-                print(bin)
                 self.op2=-(int(bin[3])*1+int(bin[2])*2+int(bin[1])*4+int(bin[0])*8)    #converting to decimal            
             else:
                 self.op2 =int((a[4])[3])*1+int((a[4])[2])*2+int((a[4])[1])*4+int((a[4])[0])*8   #converting to decimal
-        print(self.op2)
-        
         
         index =int((a[5])[2])*1+int((a[5])[1])*2+int((a[5])[0])*4
         self.op3=Main.registers[index]
-        print(self.op3)
         
         index =int((a[3])[2])*1+int((a[3])[1])*2+int((a[3])[0])*4
         
