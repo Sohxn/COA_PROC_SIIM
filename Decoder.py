821e180c144b8b8a020f3e33d98a6f6283da71f1
Stuti230904 <86624764+Stuti230904@users.noreply.github.com>
2023-11-26 07:00:26 +0530

    Update Decoder.py

--- Decoder.py
+++ Decoder.py
@@ -1,26 +1,20 @@
-import ALU
+from ALU import ALU
+from datatransfer import datatransfer
+
 
 class decoder:
-    def __init__(self,instruction):
-        #considering instruction string
-        i = [] #141343
-        i.append(instruction[0]) #alu/data_transfer
-        i.append(instruction[1:5]) #opcode
-        i.append(instruction[5]) #1 bit code 
-        i.append(instruction[6:9]) #op1 (destination)
-        i.append(instruction[9:13]) #op2
-        i.append(instruction[13:]) #op3
-        
-        if i[0] == '0':
-            obj=ALU.ALU(i)
+    def __init__(self, instruction, data, register):
+        # considering instruction string
+        i = []  # 141343
+        i.append(instruction[0])  # alu/data_transfer
+        i.append(instruction[1:5])  # opcode
+        i.append(instruction[5])  # op2 type
+        i.append(instruction[6:9])  # op1 (destination)
+        i.append(instruction[9:13])  # op2
+        i.append(instruction[13:])  # op3
+        if i[0] == "0":  # to be sent to ALU
+            ALU(i, register)  # instruction is sent to ALU
+        elif i[0] == "1":  # to be sent to ALU
+            datatransfer(i, data, register)  # instruction is sent to datatransfer
         else:
-            #send i to data_transfer
-            
-        
-        
-    
-
-            
-        
-        
-        
+            print("The instruction type is invalid")
